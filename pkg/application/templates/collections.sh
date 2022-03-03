f0fc7b2b7b2b6b75a2e184cf4fec493d12d66f6e
Warren Amphlett <warrenamphlett@gmail.com>
2022-03-03 09:37:06 +0000

    fix collection template

--- pkg/application/templates/collections.sh
+++ pkg/application/templates/collections.sh
@@ -1,6 +1,6 @@
-COLLECTIONS_PATH=$HOME/.dot-collections-2
+COLLECTIONS_PATH=$HOME/.dot-collections
 
-# Go through all the collections.sh and load the dot files within
+# Go through all the collections and load the dot files within
 for collection in "$COLLECTIONS_PATH"/*; do
   if [ -d "$collection" ]; then
     if [ -f "$collection"/collection.sh ]; then
